063ace1cc41ef364f5c88fcdfe50b4ecddab4b88
Danielly Costa <daniellycosta97@gmail.com>
2017-11-01 10:03:12 -0300

    Conexões feitas
    Falta testar

--- Cliente_Produtor/mainwindow.cpp
+++ Cliente_Produtor/mainwindow.cpp
@@ -2,54 +2,72 @@
 #include "ui_mainwindow.h"
 #include <QDateTime>
 #include<QLineEdit>
+#include<QTextBrowser>
+#include<QTimer>
+#include<QLabel>
 
 MainWindow::MainWindow(QWidget *parent) :
-  QMainWindow(parent), ui(new Ui::MainWindow){
-  ui->setupUi(this);
-  socket = new QTcpSocket(this);
-  tcpConnect();
+    QMainWindow(parent), ui(new Ui::MainWindow){
+    ui->setupUi(this);
+    socket = new QTcpSocket(this);
+    tcpConnect();
 
-  connect(ui->pushButtonPut,
-          SIGNAL(clicked(bool)),
-          this,
-          SLOT(putData()));
-  connect(ui->pushButton_connect,
-          SIGNAL(clicked(bool)),
-          this,
-          SLOT(tcpConnect()));
+    connect(ui->pushButtonPut,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(putData()));
+    connect(ui->pushButton_connect,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(tcpConnect()));
+    connect(ui->pushButton_stop,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(stopData());
 }
 //host default "127.0.0.1"
 
 void MainWindow::tcpConnect(){
-  socket->connectToHost(getHost(),1234);
-  if(socket->waitForConnected(3000)){
-    qDebug() << "Connected";
-  }
-  else{
-    qDebug() << "Disconnected";
-  }
+    socket->connectToHost(getHost(),1234); //PODE FUNÇÃO AQUI?
+    if(socket->waitForConnected(3000)){
+        qDebug() << "Connected";
+    }
+    else{
+        qDebug() << "Disconnected";
+    }
 }
+
 
 void MainWindow::putData(){
-  QDateTime datetime;
-  QString str;
-  qint64 msecdate;
+    QDateTime datetime;
+    QString str,timeStr;
+    qint64 msecdate;
+    QTextBrowser textbox;
+    //QTimer timer;
 
-  if(socket->state()== QAbstractSocket::ConnectedState){
 
-    msecdate = QDateTime::currentDateTime().toMSecsSinceEpoch();
-    str = "set "+ QString::number(msecdate) + " " + QString::number(qrand()%35)+"\r\n";
+    timeStr = ui->label_time->text(); //É ASSIM MESMO?
+    timer.start((time.toInt())*1000);
 
-      qDebug() << str;
-      qDebug() << socket->write(str.toStdString().c_str()) << " bytes written";
-      if(socket->waitForBytesWritten(3000)){
-        qDebug() << "wrote";
-      }
-  }
+    while(timer.isActive()){ //CONFERIR
+
+        if(socket->state()== QAbstractSocket::ConnectedState){
+
+            msecdate = QDateTime::currentDateTime().toMSecsSinceEpoch();
+            str = "set "+ QString::number(msecdate) + " " + QString::number(qrand()%35)+"\r\n";
+            textbox.insertPlainText(str); //CONFERIR
+
+
+            qDebug() << str;
+            qDebug() << socket->write(str.toStdString().c_str()) << " bytes written";
+            if(socket->waitForBytesWritten(3000)){
+                qDebug() << "wrote";
+            }
+        }
+    }
 }
 
-QString MainWindow::getHost(){
-    QLineEdit host;
+QString MainWindow::getHost(){ //NÃO ESTÁ FUNCIONANDO
     QString ip;
 
     ip = ui->lineEdit_Host->text();
@@ -58,7 +76,11 @@ QString MainWindow::getHost(){
 
 }
 
+void MainWindow::stopData(){ //CONFERIR
+    timer.stop();
+}
+
 MainWindow::~MainWindow(){
-  delete socket;
-  delete ui;
+    delete socket;
+    delete ui;
 }
