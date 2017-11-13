2a4aa2d8e40fa8d2ddbe1e39a5d571a39cebcd4c
Danielly Costa <daniellycmcosta@gmail.com>
2017-11-13 07:25:55 -0300

    Mensagem de erro adicionada

--- Cliente_Produtor/mainwindow.cpp
+++ Cliente_Produtor/mainwindow.cpp
@@ -6,14 +6,12 @@
 #include<QTimer>
 #include<QLabel>
 #include<QLCDNumber>
-
-//CONFIGURAR DIALOGO DE ERRO QDO O MINIMO FOR MAIOR QUE O MAXIMO
-
+ 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
     socket = new QTcpSocket(this);
-
+ 
     connect(ui->pushButtonPut,
             SIGNAL(clicked(bool)),
             this,
@@ -31,8 +29,7 @@ MainWindow::MainWindow(QWidget *parent) :
             this,
             SLOT(tcpDisconnect()));
 }
-//default host "127.0.0.1"
-
+ 
 void MainWindow::tcpConnect(){
     socket->connectToHost(getHost(),1234);
     if(socket->waitForConnected(3000)){
@@ -42,53 +39,61 @@ void MainWindow::tcpConnect(){
         qDebug() << "Disconnected";
     }
 }
-
+ 
 void MainWindow::tcpDisconnect(){
     socket->disconnectFromHost();
 }
-
-
+ 
+ 
 void MainWindow::putData(){
     QString timeStr;
-
+ 
     timeStr = ui->label_time->text();
     idTimer = startTimer((timeStr.toInt())*1000);
-
+ 
 }
-
+ 
 QString MainWindow::getHost(){
     QString ip;
-
+ 
     ip = ui->lineEdit_Host->text();
-
+ 
     return ip;
-
+ 
 }
-
-void MainWindow::stopData(){ //CONFERIR
-     killTimer(idTimer);
+ 
+void MainWindow::stopData(){
+    killTimer(idTimer);
 }
-
+ 
 void MainWindow::timerEvent(QTimerEvent *e){
     QString str;
     int max,min;
-
+    qint64 msecdate;
+ 
     max = ui->lcdNumber_max->value();
     min = ui->lcdNumber_min->value();
-
-    if(socket->state()== QAbstractSocket::ConnectedState){
-
-        str = "set "+ QDateTime::currentDateTime().toString() + " " + QString::number(qrand()%(max - min) + min)+"\r\n";
-        ui->textBrowser->insertPlainText(str + "\n");
-
-        qDebug() << str;
-        qDebug() << socket->write(str.toStdString().c_str()) << " bytes written";
-        if(socket->waitForBytesWritten(3000)){
-            qDebug() << "wrote";
+ 
+    if(min>max){
+        ui->textBrowser->insertPlainText("ERRO! VALOR MÍNIMO MAIOR QUE O MÁXIMO\n");
+        killTimer(idTimer);
+    }
+    else{
+        if(socket->state()== QAbstractSocket::ConnectedState){
+            msecdate = QDateTime::currentDateTime().toMSecsSinceEpoch();
+ 
+            str = "set "+ QString::number(msecdate) + " " + QString::number(qrand()%(max - min) + min)+"\r\n";
+            ui->textBrowser->append(str);
+ 
+            qDebug() << str;
+            qDebug() << socket->write(str.toStdString().c_str()) << " bytes written";
+            if(socket->waitForBytesWritten(3000)){
+                qDebug() << "wrote";
+            }
         }
     }
 }
-
+ 
 MainWindow::~MainWindow(){
     delete socket;
     delete ui;
